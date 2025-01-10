1a1542c4c884c843b4f2689f2964a51e4c6a0539
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-10 16:32:40 +0530

    jjj

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
          <p><FaPhoneAlt/> Phone: <a href="tel:+1234567890">+123 456 7890</a></p>
       <p><MdEmail/> Email: <a href="mailto:support@navbharatniwas.com">support@navbharatniwas.com</a></p>
       <p><HiOfficeBuilding/>  Address: Sector 63, Noida, Uttar Pradesh - 208003</p>
-      <p> <FaLocationDot/> Office Location: <a href='##'>Location</a></p>
+      <p> <FaLocationDot/> Office Location: <a href='https://www.google.com/maps/place/NavBharat+Niwas+Pvt.+Ltd./@28.616352,77.378848,17z/data=!3m1!4b1!4m6!3m5!1s0x390cefff12bff2e7:0x1197b8158b9e59f5!8m2!3d28.616352!4d77.378848!16s%2Fg%2F11wxw_nntg?entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D'>Location</a></p>
         <p>About Us</p>
         <p>Contact</p>
       </div>
