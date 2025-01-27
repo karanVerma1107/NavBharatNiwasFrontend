f79fdadc099b25f277157a1b1db55abfaf515121
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-27 16:49:42 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy:{
       '/api':{
-        target: 'https://navbharatniwasbackend.onrender.com',
+        target: 'https://www.navbharatniwas.in',
         changeOrigin: true,
         rewrite: (path)=>path.replace(/^\/api/,'/api')
       }
