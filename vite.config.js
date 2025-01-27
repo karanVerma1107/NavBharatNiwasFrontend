7fc5adaf73ae2fe4f4f9407df1b01d86acb1785d
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-28 00:23:52 +0530

    front ios

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy:{
       '/api':{
-        target: 'https://navbharatniwasbackend.onrender.com',
+        target: 'http://localhost:3007',
         changeOrigin: true,
         rewrite: (path)=>path.replace(/^\/api/,'/api')
       }
